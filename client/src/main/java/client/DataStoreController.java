dc3aaf4ad71ddc14ddc6fbb606a7a6feb1ae8439
kovsi <kovsiun@gmail.com>
2022-04-25 20:12:55 +0200

    Initial commit

---
+++ client/src/main/java/client/DataStoreController.java
@@ -0,0 +1,42 @@
+package client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+public class DataStoreController {
+
+        @FXML
+        private TextField consoleDataStore;
+
+        @FXML
+        private ListView<?> listOfData;
+
+        @FXML
+        void onBtnDelete(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onBtnDownload(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onBtnRename(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onBtnUpdate(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onBtnUpload(ActionEvent event) {
+
+        }
+}
